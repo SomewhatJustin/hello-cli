ac91c63d12e4ac6ebea03d9008ebc0bb1e1bd8a7
Justin Fowler <justinfowler1996@gmail.com>
2025-08-30 17:54:40 -0500

    clean up

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use std::io;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Post {
+struct Todo {
     #[serde(rename = "userId")]
     user_id: i32,
     id: i32,
@@ -22,12 +22,11 @@ async fn main() {
     println!("{reversed_string}");
 
     println!("Next: let's get some text from the internet");
-    let internet_text = fetch_json("https://jsonplaceholder.typicode.com/todos/1")
+    let internet_todo = fetch_todo("https://jsonplaceholder.typicode.com/todos/1")
         .await
         .unwrap();
-    // println!("some text: {internet_text}")
-    let deserialized: Post = serde_json::from_str(&internet_text).unwrap();
-    println!("deserialized = {:?}", deserialized)
+
+    println!("todo: {:?}", internet_todo)
 }
 
 fn reverse_string(string: &str) -> String {
@@ -38,10 +37,12 @@ fn reverse_string(string: &str) -> String {
     String::from(reversed_string)
 }
 
-async fn fetch_json(url: &str) -> Result<String, reqwest::Error> {
-    let body = reqwest::get(url).await?.text().await?;
+async fn fetch_todo(url: &str) -> Result<Todo, reqwest::Error> {
+    let body: String = reqwest::get(url).await?.text().await?;
 
     println!("text is {body}");
 
-    Ok(body)
+    let deserialized: Todo = serde_json::from_str(&body).expect("Failed to deserialize");
+
+    Ok(deserialized)
 }
