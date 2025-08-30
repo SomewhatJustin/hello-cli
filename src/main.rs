08b46adb8c0b09f9e10e19e7f867d18ebd6c7189
Justin Fowler <justinfowler1996@gmail.com>
2025-08-30 10:20:01 -0500

    deserialize me captain

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use serde::{Deserialize, Serialize};
 use std::io;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Post {
+    #[serde(rename = "userId")]
+    user_id: i32,
+    id: i32,
+    title: String,
+    completed: bool,
+}
 
 #[tokio::main]
 async fn main() {
@@ -12,8 +22,12 @@ async fn main() {
     println!("{reversed_string}");
 
     println!("Next: let's get some text from the internet");
-    let internet_text = fetch_json("https://swapi.info/api/films").await.unwrap();
-    println!("some text: {internet_text}")
+    let internet_text = fetch_json("https://jsonplaceholder.typicode.com/todos/1")
+        .await
+        .unwrap();
+    // println!("some text: {internet_text}")
+    let deserialized: Post = serde_json::from_str(&internet_text).unwrap();
+    println!("deserialized = {:?}", deserialized)
 }
 
 fn reverse_string(string: &str) -> String {
